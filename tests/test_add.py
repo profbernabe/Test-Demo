b7ac736aec96d48b3725935858610a664bcaa51d
profbernabe <166169531+profbernabe@users.noreply.github.com>
2026-02-03 17:40:26 -0800

    Update test_add.py

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,1 +1,16 @@
+from main import *
 
+def test_add_positive_numbers():
+    assert add(1, 2) == 3
+
+
+def test_add_with_zero():
+    assert add(1, 0) == 1
+
+
+def test_add_negative_number():
+    assert add(2, -3) == -1
+
+
+def test_add_two_negatives():
+    assert add(-2, -5) == -7

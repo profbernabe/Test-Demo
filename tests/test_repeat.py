ef9250acffab89c6d2b8b08fb2aab5b296ccd210
profbernabe <166169531+profbernabe@users.noreply.github.com>
2026-02-03 17:40:43 -0800

    Add tests for repeat_word function

--- tests/test_repeat.py
+++ tests/test_repeat.py
@@ -1,1 +1,14 @@
+from main import *
+
+
+def test_repeat_multiple_times():
+    assert repeat_word("hi", 3) == "hihihi"
+
+
+def test_repeat_once():
+    assert repeat_word("ha", 1) == "ha"
+
+
+def test_repeat_longer_word():
+    assert repeat_word("hello", 2) == "hellohello"
 
